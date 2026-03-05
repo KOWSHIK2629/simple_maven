6fcaf1036d410d6185901b467fba70af7abce3f4
KOWSHIK2629 <murugesankowshik29@gmail.com>
2026-03-05 21:51:24 +0530

    Add unit tests for calculateGrade in AppTest
    Added unit tests for calculateGrade method.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,9 @@
 package com.example;
-public class App {
-public int add(int a, int b) {
-return a + b;
+
+import static org.junit.Assert.assertEquals; import org.junit.Test;
+
+public class AppTest { @Test
+public void testCalculateGrade() {
+assertEquals("A", App.calculateGrade(95)); assertEquals("B", App.calculateGrade(85)); assertEquals("C", App.calculateGrade(75)); assertEquals("D", App.calculateGrade(65)); assertEquals("F", App.calculateGrade(50));
 }
 }
